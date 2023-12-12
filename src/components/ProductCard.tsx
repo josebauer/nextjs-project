434b88d28471b5ba26cd36ea222c64ec2e06b5b1
José Bauer <jose.h.bauer03@gmail.com>
2023-12-12 19:12:41 -0300

    Inclui componente de card e toast

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+import { ProductType } from "./services/products"
+import { Button, Card, CardBody, CardSubtitle } from "react-bootstrap"
+import Link from "next/link"
+import Image from "next/image"
+import SuccessToast from "./SuccessToast"
+
+type ProductCardProps = {
+  product: ProductType
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const [toastIsOpen, setToastIsOpen] = useState(false)
+  const { id, name, imageUrl, price } = product
+
+  return (
+    <>
+      <Card>
+        <Link href={`/products/${id}`}>
+          <Image className="card-img-top" src={imageUrl} alt={product.name} height={500} width={600}/>
+        </Link>
+
+        <CardBody>
+          <Link href={`/products/${id}`}>
+            <h5 className="card-title" style={{ cursor: 'pointer' }}>
+              {name}
+            </h5>
+          </Link>
+
+          <CardSubtitle className="mb-3 text-muted">
+            R$ {price}
+          </CardSubtitle>
+
+          <Button
+            className="pb-2"
+            onClick={() => {
+              setToastIsOpen(true)
+              setTimeout(() => setToastIsOpen(false), 1000 * 3)
+            }}
+          >
+            Adicionar ao Carrinho
+          </Button>
+        </CardBody>
+      </Card>
+
+      <SuccessToast toastIsOpen={toastIsOpen} setToastIsOpen={setToastIsOpen}/>
+    </>
+  )
+  
+}
--- src/components/services/products.ts
+++ src/components/services/products.ts
@@ -3,7 +3,7 @@ export type ProductType = {
   name: string
   description: string
   price: number
-  imageURL: string
+  imageUrl: string
   inStock: number
 }
 
