df2c19402103ad6387b9763dfd667548230b7b66
José Bauer <jose.h.bauer03@gmail.com>
2023-12-13 18:22:00 -0300

    Cria componente CartTable e adiciona na página Cart

---
+++ src/components/CartTable.tsx
@@ -0,0 +1,100 @@
+import { useCart } from "@/hooks/UseCart"
+import { ProductType } from "./services/products"
+import { Button, Col, Row, Table } from "react-bootstrap"
+import Image from "next/image"
+import { useEffect, useState } from "react"
+
+type CartEntry = {
+  product: ProductType
+  quantity: number
+}
+
+const CartTableRow = (props: {
+  entry: CartEntry
+}) => {
+  const { addProduct, removeProduct } = useCart()
+
+  return (
+    <tr>
+      <td>
+        <Row className="align-items-center">
+          <Col>
+            <Image
+              src={props.entry.product.imageUrl}
+              alt={props.entry.product.name}
+              height={50}
+              width={70}
+            />
+          </Col>
+          <Col xs={8} md={10}>
+            {props.entry.product.name}
+          </Col>
+        </Row>
+      </td>
+      <td>R$ {props.entry.product.price}</td>
+      <td>{props.entry.quantity}</td>
+      <td>R$ {(props.entry.product.price * props.entry.quantity)}</td>
+      <td>
+        <Button
+          size="sm"
+          onClick={() => addProduct(props.entry.product)}
+        >
+          +
+        </Button>
+        {' '}
+        <Button
+          className="btn-danger"
+          size="sm"
+          onClick={() => removeProduct(props.entry.product.id)}
+        >
+          -
+        </Button>
+      </td>
+    </tr>
+  )
+}
+
+export default function CartTable() {
+  const [cartEntries, setCartEntries] = useState<CartEntry[]>([])
+  const { cart } = useCart()
+
+  useEffect(() => {
+    const entriesList = cart.reduce((list, product) => {
+      const entryIndex = list.findIndex(entry => entry.product.id === product.id)
+
+      if (entryIndex === -1) {
+        return [
+          ...list,
+          {
+            product,
+            quantity: 1
+          }
+        ]
+      }
+
+      list[entryIndex].quantity++
+      return list
+
+    }, [] as CartEntry[])
+
+    entriesList.sort((a, b) => a.product.id - b.product.id)
+    setCartEntries(entriesList)
+
+  }, [cart])
+
+  return (
+    <Table responsive className="align-middle" style={{ minWidth: '32rem' }}>
+      <thead>
+        <tr>
+          <th>Produto</th>
+          <th>Preço</th>
+          <th>Qtd.</th>
+          <th>Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        {cartEntries.map(entry => <CartTableRow key={entry.product.id} entry={entry} />)}
+      </tbody>
+    </Table>
+  )
+}
--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,5 +1,7 @@
+import CartTable from '@/components/CartTable'
 import Header from '@/components/Header'
 import Head from 'next/head'
+import { Container } from 'react-bootstrap'
 
 export default function Cart() {
   return (
@@ -12,7 +14,15 @@ export default function Cart() {
 
       <Header />
 
-      <h1>Carrinho</h1>
+      <main>
+        <Container className='mb-5'>
+          <h1 className='my-5'>
+            Carrinho
+          </h1>
+
+          <CartTable />
+        </Container>
+      </main>
     </>
   )
 }
