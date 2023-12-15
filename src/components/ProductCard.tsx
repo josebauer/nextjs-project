dffcc59e784ec2a73e2aca4ccb23711e08248634
José Bauer <jose.h.bauer03@gmail.com>
2023-12-15 19:55:22 -0300

    Altera lista de produtos, largura dos cards e o arquivo next.config

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -19,7 +19,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
     <>
       <Card>
         <Link href={`/products/${id}`}>
-          <Image className="card-img-top" src={imageUrl} alt={product.name} height={250} width={600}/>
+          <Image className="card-img-top" src={imageUrl} alt={product.name} height={250} width={500}/>
         </Link>
 
         <Card.Body>
