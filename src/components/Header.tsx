e0223e43e1cd448b15f0bf5bdd2663f2905d2c27
José Bauer <jose.h.bauer03@gmail.com>
2023-12-21 15:12:24 -0300

    Altera navbar e adiciona visual da página de login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,34 @@
+import Image from "next/image";
 import Link from "next/link";
 import { Container, Nav, Navbar } from "react-bootstrap";
 
 export default function Header() {
   return (
-    <Navbar expand="lg" className="bg-primary" data-bs-theme="dark">
+    <Navbar expand="lg" className="bg-primary py-3" data-bs-theme="dark">
       <Container>
         <Link href="/" className="navbar-brand">
           Tech Shop
         </Link>
-        <Nav>
-          <Link href="/" className="nav-link">
-            Início
-          </Link>
-          <Link href="/products" className="nav-link">
+        <Nav className="align-items-center h6 g-4">
+          <div className="account">
+            <div className="d-flex text-light">
+              Faça o
+              <Link href="/login" className="nav-link py-0">
+                login
+              </Link>
+            </div>
+            <div className="d-flex text-light">
+              ou
+              <Link href="/register" className="nav-link py-0">
+                cadastre-se
+              </Link>
+            </div>
+          </div>
+          <Link href="/products" className="nav-link pe-3">
             Produtos
           </Link>
-          <Link href="/cart" className="nav-link">
+          <Link href="/cart" className="d-flex align-items-center nav-link border border-white rounded">
+            <Image className="me-2" src='/cart.png' alt='carrinho de compras' width={30} height={30} />
             Carrinho
           </Link>
         </Nav>
